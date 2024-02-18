18aa021cd362295c27bef01ba63907dfe75729f4
Esther <fuest8456258@gmail.com>
2024-02-18 09:33:50 +0800

    first

---
+++ src/js/main.js
@@ -0,0 +1,42 @@
+// icon
+import "material-symbols";
+// bootstrap
+// Import our custom CSS
+import "../scss/style.scss";
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+const navbarMenuBtn = document.querySelector(".navbar-toggler");
+
+navbarMenuBtn.addEventListener("click", (e) => {
+  const menuBtn = e.target;
+  if (menuBtn.classList.contains("navbar-toggler-menu")) {
+    menuBtn.classList.toggle("close");
+    menuBtn.textContent = menuBtn.classList.contains("close")
+      ? "close"
+      : "menu";
+  }
+});
+//表單驗證
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
