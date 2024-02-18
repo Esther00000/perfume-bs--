85dc2c84dffb4888815468156d6027c4530c2f26
Esther <fuest8456258@gmail.com>
2024-02-18 12:12:06 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+import { glob } from "glob";
 
 export default defineConfig({
   root: resolve(__dirname, "src"),
   base: "/perfume-bs-Layout/",
   build: {
     outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: Object.fromEntries(
+        glob
+          .sync("*.html")
+          .map(
+            (file) => [file.replace(/\.html$/, "")],
+            fileURLToPath(new URL(file, import.meta.url))
+          )
+      ),
+    },
   },
   server: {
     open: "/",
